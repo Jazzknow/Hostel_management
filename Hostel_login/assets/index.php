fc0d8b54ac6acfe0bd31d1faf34962f26edde024
Jasper <147628504+Jazzknow@users.noreply.github.com>
2024-09-04 18:09:49 +0800

    Add files via upload

---
+++ Hostel_login/assets/index.php
@@ -0,0 +1,105 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <link rel="icon" href="img/logo_tab.png" type="x-icon">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hostel Management | Login</title>
+    <link rel="stylesheet" href="../assets/css/style2.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+
+</head>
+
+<body>
+    <div class="container">
+        <h1 class="form-title">Login Form</h1>
+        <form id="form" class="form" method="POST">
+            <div class="main-user-info">
+                <div class="user-input-box">
+                    <label for="email">Email</label>
+                    <input type="email" id="email" name="email" placeholder="Enter email" autocomplete="email">
+                    <small>Error Message</small>
+                </div>
+                <div class="user-input-box">
+                    <label for="password">Password</label>
+                    <div class="password-container">
+                        <input type="password" id="password" name="password" placeholder="Enter password"
+                            autocomplete="new-password">
+                        <i class="fas fa-eye password-toggle"></i>
+                    </div>
+                    <small>Error Message</small>
+                </div>
+                <script>
+                    document.addEventListener('DOMContentLoaded', function () {
+                        const passwordInput = document.getElementById('password');
+                        const passwordToggle = document.querySelector('.password-toggle');
+
+                        passwordToggle.addEventListener('click', function () {
+                            if (passwordInput.type === 'password') {
+                                passwordInput.type = 'text';
+                                passwordToggle.classList.remove('fa-eye');
+                                passwordToggle.classList.add('fa-eye-slash');
+                            } else {
+                                passwordInput.type = 'password';
+                                passwordToggle.classList.remove('fa-eye-slash');
+                                passwordToggle.classList.add('fa-eye');
+                            }
+                        });
+                    });
+                </script>
+            </div>
+            <div class="remember-forgot">
+                <label class="remember-me">
+                    <input type="checkbox" id="remember" name="remember"> Remember me
+                </label>
+                <a href="#" class="forgot-password">Forgot Password?</a>
+            </div>
+            <div class="form-submit-btn">
+                <input type="submit" value="Login">
+            </div>
+        </form>
+        <div class="signup-redirect">
+            <p>Don't have an account? <a href="registration.php">Sign up</a></p>
+        </div>
+    </div>
+</body>
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "hostel_db";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+    echo $conn->connect_error;
+}
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+}
+
+$sql = "SELECT * FROM user_tbl WHERE email = '$email'";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    $row = $result->fetch_assoc();
+    $hashed_password = $row['password'];
+
+    if (password_verify($hashed_password, $password)) {
+        echo '<script>alert("Login Successfully");</script>';
+        echo '<script>window.location.href="#";</script>';
+    } else {
+        echo '<script>alert("Invalid Email or Password");</script>';
+        echo '<script>window.location.href="index.php";</script>';
+    }
+} else {
+    echo "error";
+}
+
+$conn->close();
+?>
+
+</html>
---
+++ Hostel_login/assets/sql/hostel_db.sql
@@ -0,0 +1,70 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Sep 04, 2024 at 11:49 AM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `hostel_db`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_tbl`
+--
+
+CREATE TABLE `user_tbl` (
+  `id` int(50) NOT NULL,
+  `fullname` varchar(100) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
+  `email` varchar(100) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
+  `phone` varchar(50) NOT NULL,
+  `password` varchar(50) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
+  `gender` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `user_tbl`
+--
+
+INSERT INTO `user_tbl` (`id`, `fullname`, `email`, `phone`, `password`, `gender`) VALUES
+(46, 'Jasper Miclat', 'miclatj2@yahoo.com', '961 447 3138', '$2y$10$chgEzMMkTpI1iHkfZABl..BUJfn4N7svpI2UbfWykBG', 'male'),
+(47, 'Aaron', 'aaron@gmail.com', '961 447 3138', '$2y$10$g3rgUJAcXY7W3gK.xpgEl.RGoOw2wkFQfTuYKnbLvFi', 'male');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `user_tbl`
+--
+ALTER TABLE `user_tbl`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `user_tbl`
+--
+ALTER TABLE `user_tbl`
+  MODIFY `id` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=48;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
